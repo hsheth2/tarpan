bb9c240de8290879e363f00e4d63ced4af0ff36d
Harshal Sheth <hsheth2@gmail.com>
2017-02-26 20:34:21 -0500

    Add BGP tarpan attr type code

--- bgpd/bgp_attr.c
+++ bgpd/bgp_attr.c
@@ -1988,6 +1988,9 @@ bgp_attr_parse (struct peer *peer, struct attr *attr, bgp_size_t size,
 	case BGP_ATTR_EXT_COMMUNITIES:
 	  ret = bgp_attr_ext_communities (&attr_args);
 	  break;
+	case BGP_ATTR_TARPAN:
+	  // TODO: call a method
+	  break;
 	default:
 	  ret = bgp_attr_unknown (&attr_args);
 	  break;
