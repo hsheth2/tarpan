f86e305c6f3bdafb98bcc859594fd6296c548ee0
Harshal Sheth <hsheth2@gmail.com>
2017-04-19 18:05:05 -0400

    quality test suite

--- bgpd/tarpan_test.cc
+++ bgpd/tarpan_test.cc
@@ -6,9 +6,11 @@
  */
 
 #include <iostream>
+#include <algorithm>
 
 int main(int argc, char **argv) {
   // TODO: add actual unit tests
+  std::cout << "All tests passed." << std::endl;
   return 0;
 }
 
