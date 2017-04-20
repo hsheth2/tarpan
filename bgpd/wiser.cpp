e70b5ac556ed6f891bf86f35fb182a3291e70bd4
Harshal Sheth <hsheth2@gmail.com>
2017-04-20 14:32:18 -0400

    Added functions for cost normalization

--- bgpd/wiser.cpp
+++ bgpd/wiser.cpp
@@ -33,13 +33,55 @@ struct tarpan_protocol_handler wiser_protocol_handler = {
     .initialize_packet = wiser_initialize_packet,
 };
 
+void wiser_costs_table_init();
+
 void wiser_protocol_init(void)
 {
   zlog_info("wiser_protocol_init: starting");
 
-  // TODO initialize wiser costs table (both send and recv)
+  // TODO read costs file
+
+  wiser_costs_table_init();
 
   // TODO open cost portal
+}
+
+static std::map<as_t, uint32_t> advcost_sent;
+static std::map<as_t, uint32_t> advcost_recv;
+
+// initialize wiser costs table (both send and recv)
+void wiser_costs_table_init()
+{
+  advcost_sent.clear();
+  advcost_recv.clear();
+}
+
+void increment_map_value(std::map<as_t, uint32_t>& map, as_t key, uint32_t delta)
+{
+  auto find = map.find(key);
+
+  if (find == map.end()) {
+      map[key] = delta;
+  } else
+    find->second += delta;
+}
+
+void update_sent_cost(as_t destination, uint32_t cost) {
+  increment_map_value(advcost_sent, destination, cost);
+}
+
+void update_recv_cost(as_t from, uint32_t cost) {
+  increment_map_value(advcost_recv, from, cost);
+}
+
+double normalization(as_t peer) {
+  // multiply the costs received from this peer by the normalization factor
+
+  // TODO: come up with a better system for handling 0
+  uint32_t total_send = 1+advcost_sent[peer];
+  uint32_t total_recv = 1+advcost_recv[peer];
+
+  return (double)total_send / (double) total_recv;
 }
 
 void wiser_packet_received_handler (struct peer *const peer,
@@ -79,23 +121,23 @@ void wiser_initialize_packet (struct peer *const peer,
   wiser__init(wiser);
   tarpan->message->wiser = wiser;
 
-  struct in_addr local_ip = peer->bgp->router_id;
-  as_t as_number = peer->bgp->as;
-
   wiser->path_cost = 5; // TODO: lookup in static file
 
-  // TODO update cost portal data
+  update_sent_cost(peer->as, wiser->path_cost);
 
   zlog_debug("wiser_initialize_packet");
 
   // place this node's AS/IP address in wiser data
   IPAddress* local_addr = (IPAddress*) malloc(sizeof(IPAddress));
   ipaddress__init(local_addr);
-  local_addr->bytes = local_ip.s_addr;
+  local_addr->bytes = peer->bgp->router_id.s_addr;
 
-  wiser->sender_as = as_number;
+  wiser->sender_as = peer->bgp->as;
   wiser->sender_address = local_addr;
 }
+
+// TODO wiser_forward_packet (or something)
+// ^ should use normalization?
 
 void wiser_best_selection (struct bgp *bgp, struct bgp_node *rn,
   		    struct bgp_maxpaths_cfg *mpath_cfg,
