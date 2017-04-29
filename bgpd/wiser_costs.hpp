2b380bede5b7ee25675123f2b896579d0fcc8925
Harshal Sheth <hsheth2@gmail.com>
2017-04-28 22:54:15 -0400

    add socket and threading code
    TODO: interact with the incoming data from the socket

--- bgpd/wiser_costs.hpp
+++ bgpd/wiser_costs.hpp
@@ -12,6 +12,11 @@
 #include <unordered_map>
 #include <mutex>
 #include <thread>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
 
 static std::mutex cost_mutex;
 static std::thread wiser_cost_portal_thread;
@@ -50,13 +55,59 @@ static void update_recv_cost(as_t from, uint32_t cost) {
   increment_map_value(advcost_recv, from, cost);
 }
 
-static void cost_portal()
+static void cost_portal_handle_connection(int socket)
 {
-  // TODO
+  int size;
+  uint8_t buffer[1024];
+  size = read( socket , buffer, 1024);
+
+  // TODO: interact with incoming data
+  printf("%s\n",buffer );
+  printf("Hello message read from socket\n");
+
+  close(socket);
+}
+
+static int cost_portal(int server_fd, struct sockaddr_in address)
+{
+  int addrlen = sizeof(address);
+  int new_socket;
+
+  while ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+			      (socklen_t*)&addrlen))) {
+      if (new_socket < 0)
+	break;
+
+      std::thread(cost_portal_handle_connection, new_socket);
+  }
+
+  close(server_fd);
+  return 0;
 }
 
 int wiser_cost_portal_init() {
-  wiser_cost_portal_thread = std::thread(cost_portal);
+  int server_fd;
+  struct sockaddr_in address;
+  int opt = 1;
+
+  // Creating socket file descriptor
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    return 1;
+
+  // Forcefully attaching socket to port
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+    return 1;
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons( wiser_cost_portal_port );
+
+  // Forcefully attaching socket to the port
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    return 1;
+  if (listen(server_fd, 3) < 0)
+    return 1;
+
+  wiser_cost_portal_thread = std::thread(cost_portal, server_fd, address);
   return 0;
 }
 
