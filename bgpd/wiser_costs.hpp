c4e29e2b69d0e8c3d49a131c07b1112991d09596
Harshal Sheth <hsheth2@gmail.com>
2017-04-28 22:31:29 -0400

    add multithreading + protection

--- bgpd/wiser_costs.hpp
+++ bgpd/wiser_costs.hpp
@@ -10,6 +10,12 @@
 
 #include <map>
 #include <unordered_map>
+#include <mutex>
+#include <thread>
+
+static std::mutex cost_mutex;
+static std::thread wiser_cost_portal_thread;
+static const int wiser_cost_portal_port = 2259;
 
 void wiser_costs_table_init();
 int wiser_cost_portal_init();
@@ -20,14 +26,16 @@ static std::unordered_map<as_t, uint32_t> advcost_recv;
 // initialize wiser costs table (both send and recv)
 void wiser_costs_table_init()
 {
+  // technically not needed
   advcost_sent.clear();
   advcost_recv.clear();
 }
 
 static void increment_map_value(std::unordered_map<as_t, uint32_t>& map, as_t key, uint32_t delta)
 {
-  auto find = map.find(key);
+  std::lock_guard<std::mutex> guard(cost_mutex);
 
+  auto find = map.find(key);
   if (find == map.end())
     map[key] = delta;
   else
@@ -42,13 +50,19 @@ static void update_recv_cost(as_t from, uint32_t cost) {
   increment_map_value(advcost_recv, from, cost);
 }
 
+static void cost_portal()
+{
+  // TODO
+}
+
 int wiser_cost_portal_init() {
-  // TODO:
+  wiser_cost_portal_thread = std::thread(cost_portal);
   return 0;
 }
 
+// multiply the costs received from this peer by the normalization factor
 static double normalization(as_t peer) {
-  // multiply the costs received from this peer by the normalization factor
+  std::lock_guard<std::mutex> guard(cost_mutex);
 
   // TODO: come up with a better system for handling 0
   uint32_t total_send = 1+advcost_sent[peer];
